5ae3d6e476bc439a641f24f923cc3709a8d112e8
Ian Allan Connelly <ian.connelly@cern.ch>
2021-05-27 12:23:56 +0100

    update

--- MatrixUpdate.py
+++ MatrixUpdate.py
@@ -2,7 +2,7 @@ from rgbmatrix5x5 import RGBMatrix5x5
 class MatrixUpdate(RGBMatrix5x5):
 	"""Updated class to adjust some of the access to the LEDs"""
 	""" https://buildmedia.readthedocs.org/media/pdf/smbus2/latest/smbus2.pdf """
-	def _get_current_state():
+	def _get_current_state(self):
 		output = []
 		for offset in range(0,144,32):
 			output.extend ( self.i2c.read_i2c_block_data(self.address, _COLOR_OFFSET + offset, 32) )
