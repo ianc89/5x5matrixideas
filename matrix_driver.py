de6c52ffe0178ed2f99a7696c962edafa943b673
Ian Allan Connelly <ian.connelly@cern.ch>
2021-05-28 13:26:30 +0100

    Test writing a reduced driver for chip

---
+++ matrix_driver.py
@@ -0,0 +1,68 @@
+""" Class to drive the matrix """
+from matrix_properties import properties
+
+class driver():
+	def __init__(self, address=0x74):
+		self.i2c        = None
+		self.address    = address
+		self.properties = properties()
+		self.LED_GAMMA  = self.properties.LED_GAMMA
+		self.buffer     = {}
+
+	def switch_bank(self, bank=None):
+		"""Switch display driver memory bank."""
+		if bank is None:
+			return self.i2c.readfrom_mem(self.address, self.properties._BANK_ADDRESS, 1)[0]
+		self.i2c.write_i2c_block_data(self.address, self.properties._BANK_ADDRESS, [bank])
+
+	def setup(self):
+		# Connect to the bus
+		import smbus
+		self.i2c = smbus.SMBus(1)
+		# Configure the chip
+		# Switch to configuration bank
+		self.switch_bank(self.properties._CONFIG_BANK)
+		# Switch to Picture Mode
+		self.i2c.write_i2c_block_data(self.address, self.properties._MODE_REGISTER, [self.properties._PICTURE_MODE])
+		# Disable audio sync
+		self.i2c.write_i2c_block_data(self.address, self.properties._AUDIOSYNC_REGISTER, [0])
+		# Pattern (?)
+		enable_pattern = [
+		    0b00000000, 0b10111111,
+		    0b00111110, 0b00111110,
+		    0b00111111, 0b10111110,
+		    0b00000111, 0b10000110,
+		    0b00110000, 0b00110000,
+		    0b00111111, 0b10111110,
+		    0b00111111, 0b10111110,
+		    0b01111111, 0b11111110,
+		    0b01111111, 0b00000000,
+		]
+		# Loop through the banks (frames)
+		for frame in range(8):
+			self.switch_bank(frame)
+			self.i2c.write_i2c_block_data(self.address, 0x00, enable_pattern)
+
+	def activate_pixel(self, x, y, rgb):
+		# Write a function which only touches LED for single pixel
+		# Given x,y -> get the lookup index
+		addr_loc = self.properties.pixel[(x,y)]
+		# From the lookup index, get the rgb addresses
+		ir = self.properties.lookup[addr_loc][0]
+		ig = self.properties.lookup[addr_loc][1]
+		ib = self.properties.lookup[addr_loc][2]
+		# Using rgb address, write data relating to activation
+		self.i2c.write_byte_data(self.address, ir, rgb[0])
+		self.i2c.write_byte_data(self.address, ig, rgb[1])
+		self.i2c.write_byte_data(self.address, ib, rgb[2])
+
+
+	def test(self):
+		self.activate_pixel(0,0,[200,0,0])
+		
+
+
+
+
+
+
