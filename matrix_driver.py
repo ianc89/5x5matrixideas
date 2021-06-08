422fc104e3e98d50726cc786507e867a1bc52c26
Ian Allan Connelly <ian.connelly@cern.ch>
2021-06-08 15:29:11 +0100

    test

--- matrix_driver.py
+++ matrix_driver.py
@@ -1,79 +1,23 @@
 """ Class to drive the matrix """
+from adafruit_is31fl3731 import IS31FL3731
 from matrix_properties import properties
 
-class driver():
-	def __init__(self, address=0x74):
-		self.i2c        = None
-		self.address    = address
+class matrix5x5(IS31FL3731):
+	width = 5
+	height = 5
+
+	def __init__(self, i2c, address=0x74):
+		super().__init__(i2c, address)
 		self.properties = properties()
-		self.LED_GAMMA  = self.properties.LED_GAMMA
-		self.buffer     = {}
 
-	def switch_bank(self, bank):
-		"""Switch display driver memory bank."""
-		self.i2c.write_i2c_block_data(self.address, self.properties._BANK_ADDRESS, [bank])
+	def set_pixel(self,x,y,r=0,g=0,b=0,blink=None,frame=None):
+		rgb_loc = self.properties.pixel[(x,y)]
+		rgb = self.properties.lookup[rgb_loc]
+		super().pixel(rgb,0,r,blink,frame)
+		super().pixel(rgb,1,g,blink,frame)
+		super().pixel(rgb,2,b,blink,frame)
 
-	def setup(self):
-		# Connect to the bus
-		#import smbus
-		#self.i2c = smbus.SMBus(1)
-		import smbus2
-		self.i2c = smbus2.SMBus(1)
-		self.i2c.enable_pec(True)
-		# Configure the chip
-		# Switch to configuration bank
-		self.switch_bank(self.properties._CONFIG_BANK)
-		# Switch to Picture Mode
-		self.i2c.write_i2c_block_data(self.address, self.properties._MODE_REGISTER, [self.properties._PICTURE_MODE])
-		# Disable audio sync
-		self.i2c.write_i2c_block_data(self.address, self.properties._AUDIOSYNC_REGISTER, [0])
-		# Pattern (?)
-		enable_pattern = [
-		    0b00000000, 0b10111111,
-		    0b00111110, 0b00111110,
-		    0b00111111, 0b10111110,
-		    0b00000111, 0b10000110,
-		    0b00110000, 0b00110000,
-		    0b00111111, 0b10111110,
-		    0b00111111, 0b10111110,
-		    0b01111111, 0b11111110,
-		    0b01111111, 0b00000000,
-		]
-		# Loop through the banks (frames)
-		for frame in range(8):
-			self.switch_bank(frame)
-			self.i2c.write_i2c_block_data(self.address, self.properties._FRAME_REGISTER, [frame] )
-			self.i2c.write_i2c_block_data(self.address, self.properties._MODE_REGISTER, enable_pattern)
-
-	def activate_pixel(self, frame, x, y, rgb):
-		self.switch_bank(frame)
-		self.i2c.write_i2c_block_data(self.address, self.properties._FRAME_REGISTER, [frame] )
-
-		# Write a function which only touches LED for single pixel
-		# Given x,y -> get the lookup index
-		addr_loc = self.properties.pixel[(x,y)]
-		print (addr_loc)
-		# From the lookup index, get the rgb addresses
-		ir = self.properties.lookup[addr_loc][0]
-		ig = self.properties.lookup[addr_loc][1]
-		ib = self.properties.lookup[addr_loc][2]
-		print (ir,ig,ib)
-		# Using rgb address, write data relating to activation
-		self.i2c.write_byte_data(self.address, ir, rgb[0])
-		self.i2c.write_byte_data(self.address, ig, rgb[1])
-		self.i2c.write_byte_data(self.address, ib, rgb[2])
-
-
-	def test(self):
-		self.activate_pixel(1, 0,0,[200,0,0])
-		self.activate_pixel(1, 0,1,[200,0,0])
-
-
-
-		
-
-
-
-
-
-
+	@staticmethod
+	def pixel_addr(x, y):
+		# Note this required to comment out the size check inside the main driver
+		return x[y]
--- test.py
+++ test.py
@@ -1,31 +1,13 @@
-from adafruit_is31fl3731 import IS31FL3731
-from matrix_properties import properties
-
-class matrix5x5(IS31FL3731):
-	width = 5
-	height = 5
-
-	def __init__(self, i2c, address=0x74):
-		super().__init__(i2c, address)
-		self.properties = properties()
-
-	def set_pixel(self,x,y,r=0,g=0,b=0,blink=None,frame=None):
-		rgb_loc = self.properties.pixel[(x,y)]
-		rgb = self.properties.lookup[rgb_loc]
-		super().pixel(rgb,0,r,blink,frame)
-		super().pixel(rgb,1,g,blink,frame)
-		super().pixel(rgb,2,b,blink,frame)
-
-	@staticmethod
-	def pixel_addr(x, y):
-		return x[y]
+from matrix_driver import matrix5x5
 
 if __name__ == "__main__":
+	# Using busio and board to allow lock on the display
 	import busio
 	import board
-	import time
 	display = matrix5x5(busio.I2C(board.SCL, board.SDA), 0x74)
+	# Test frame switching (seems to function)
 	display.frame(2)
+	# Turns on all the LED (adjust colour with values)
 	for x in range(display.width):
 		for y in range(display.height):
 			display.set_pixel(x,y,1,20,80,frame=2)
