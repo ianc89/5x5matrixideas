9ed7a9fe38f6b8118d9ad1a395697c850e61bf1e
Ian Allan Connelly <ian.connelly@cern.ch>
2021-06-09 12:16:33 +0100

    Updating clock and display

--- clock_2.py
+++ clock_2.py
@@ -82,17 +82,17 @@ while True:
 		# Minutes
 		if b_minute[x] == "1":
 			if minute_over_30:
-				rgbmatrix5x5.set_pixel(y_minute, x, c_min_1[0], c_min_1[1], c_min_1[2], frame=0)
+				rgbmatrix5x5.set_pixel(y_minute, x, c_min_2[0], c_min2[1], c_min_2[2], frame=0)
 			else:
-				rgbmatrix5x5.set_pixel(y_minute, x, c_min_2[0], c_min_2[1], c_min_2[2], frame=0)
+				rgbmatrix5x5.set_pixel(y_minute, x, c_min_1[0], c_min_1[1], c_min_1[2], frame=0)
 		else:
 			rgbmatrix5x5.set_pixel(y_minute, x, 0, 0, 0, frame=0)
 		# Seconds
 		if b_second[x] == "1":
 			if second_over_30:
-				rgbmatrix5x5.set_pixel(y_second, x, c_sec_1[0], c_sec_1[1], c_sec_1[2], frame=0)
-			else:
 				rgbmatrix5x5.set_pixel(y_second, x, c_sec_2[0], c_sec_2[1], c_sec_2[2], frame=0)
+			else:
+				rgbmatrix5x5.set_pixel(y_second, x, c_sec_1[0], c_sec_1[1], c_sec_1[2], frame=0)
 		else:
 			rgbmatrix5x5.set_pixel(y_second, x, 0, 0, 0, frame=0)
 	# If only showing minutes
